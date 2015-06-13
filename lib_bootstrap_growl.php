49319a08905fe6eb70a437ca5454df3f2a7497e6
Messerbill <ole.hoppe@gmx.de>
2015-06-13 15:39:04 +0200

    added lib/bootstrap_growl

---
+++ lib_bootstrap_growl.php
@@ -0,0 +1,9 @@
+<?php
+namespace LIB;
+class lib_bootstrap_growl extends \LIB\lib_js{
+    public static function get_class(){
+        return self::class;}
+    public static function js_path(){
+        return \SYSTEM\WEBPATH(new \SYSTEM\PLIB(),'bootstrap-growl/lib/animate.min.css');}
+    public static function version(){
+return '<a href="https://github.com/ifightcrime/bootstrap-growl/tree/162daa41cd1155fe308ef14daea6296fbaa3d756" target="_blank">github commit</a>';}}
