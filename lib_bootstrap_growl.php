5940b96a932d940580ccf0d3ba46b4c381f90c08
Messerbill <ole.hoppe@gmx.de>
2015-06-13 16:02:34 +0200

    further path changes

--- lib_bootstrap_growl.php
+++ lib_bootstrap_growl.php
@@ -4,6 +4,6 @@ class lib_bootstrap_growl extends \LIB\lib_js{
     public static function get_class(){
         return self::class;}
     public static function js_path(){
-        return \SYSTEM\WEBPATH(new \SYSTEM\PLIB(),'bootstrap_growl/lib/animate.min.css');}
+        return \SYSTEM\WEBPATH(new \SYSTEM\PLIB(),'bootstrap_growl/lib/jquery.bootstrap-growl.min.js');}
     public static function version(){
 return '<a href="https://github.com/ifightcrime/bootstrap-growl/tree/162daa41cd1155fe308ef14daea6296fbaa3d756" target="_blank">github commit</a>';}}
